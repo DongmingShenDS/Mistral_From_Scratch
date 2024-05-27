dc0b2cedd687fa5433ff20c9d6da2e70c71ced8b
Lindsey Feng <lindseyf@ipsec-10-2-193-120.vpn.gatech.edu>
2024-05-26 23:14:24 -0400

    update

--- server/generate.py
+++ server/generate.py
@@ -80,7 +80,6 @@ def generate(
             ).cumsum(dim=0)
             - 1,
         )
-        print("generate: ", last_token_prelogits.shape)
         assert last_token_prelogits.shape == (B, V)
 
     # decode
@@ -91,7 +90,7 @@ def generate(
     print("decode", last_token_prelogits, last_token_prelogits.shape)
     for _ in range(max_tokens):
         next_token = sample(last_token_prelogits, temperature=temperature, top_p=0.8)
-        print(next_token)
+        print("last_token_prelogits", last_token_prelogits.shape)
 
         if eos_id is not None:
             is_finished = is_finished ^ (next_token == eos_id).cpu()
