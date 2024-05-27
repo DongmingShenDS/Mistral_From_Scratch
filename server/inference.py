48ef53176fa668df3e7cee6f2ed119610507fd07
Lindsey Feng <lindseyf@ipsec-10-2-193-120.vpn.gatech.edu>
2024-05-26 21:20:53 -0400

    update

--- server/inference.py
+++ server/inference.py
@@ -39,16 +39,19 @@ transformer = Transformer(
 # total_params = count_parameters(transformer)
 # print(f"Total trainable parameters in the model: {total_params}")
 
+## inference without cache
 # input = torch.tensor([args.vocab_size - 1] * (args.dim * args.max_batch_size)).to(device)
 # seqlens = [args.dim] * args.max_batch_size  # Assuming all sequences are of maximum length for simplicity
 # output = transformer(input, seqlens)
 # print(output)
 
+## inference with cache
+
 encoded_prompts = [[1, 2], [4, 5, 6]]
 generated_sequences = generate(
     encoded_prompts=encoded_prompts,
     model=transformer,
-    max_tokens=20,
+    max_tokens=1,
     temperature=0.8,
     eos_id=None  # Replace with an appropriate eos_id if available
 )
